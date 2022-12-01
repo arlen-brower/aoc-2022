a749290678536386d2a07f16a799eb441cd8769d
Arlen <arlen.brower@curtin.edu.au>
2022-12-01 16:18:57 +0800

    refactoring, reducing size

--- 1 -- Elven Calories/day_one.ex
+++ 1 -- Elven Calories/day_one.ex
@@ -1,21 +1,17 @@
 defmodule DayOne do
   def most_calories(input_file, num \\ 1) do
-    {:ok, elves} = File.read(input_file)
-
-    {top, _rest} =
-      elves
-      |> String.trim_trailing()
-      |> String.split("\n\n")
-      |> Enum.map(&parse_elf/1)
-      |> Enum.sort(:desc)
-      |> Enum.split(num)
-
-    Enum.sum(top)
+    input_file
+    |> File.read!()
+    |> String.split("\n\n", trim: true)
+    |> Enum.map(&parse_elf/1)
+    |> Enum.sort(:desc)
+    |> Enum.take(num)
+    |> Enum.sum()
   end
 
   defp parse_elf(elf) do
     elf
-    |> String.split("\n")
+    |> String.split("\n", trim: true)
     |> Enum.map(&String.to_integer/1)
     |> Enum.sum()
   end
