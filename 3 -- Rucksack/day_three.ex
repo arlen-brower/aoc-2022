b6258e7bfb7a754fb60a601e6af6b71c3269772e
Arlen <arlen.brower@curtin.edu.au>
2022-12-03 16:55:55 +0800

    Day 3, part two added

--- 3 -- Rucksack/day_three.ex
+++ 3 -- Rucksack/day_three.ex
@@ -16,15 +16,37 @@ defmodule DayThree do
           String.contains?(second, n),
           do: n
 
-    num =
-      hd(letter)
-      |> String.to_charlist()
-      |> hd
-
-    if num > 96 do
-      num - 96
-    else
-      num - 38
-    end
+    hd(letter)
+    |> String.to_charlist()
+    |> hd
+    |> to_priority()
   end
+
+  def badges(file_path) do
+    File.read!(file_path)
+    |> String.split("\n", trim: true)
+    |> Enum.chunk_every(3)
+    |> Enum.map(&get_badge/1)
+    |> Enum.sum()
+  end
+
+  defp get_badge(group) do
+    badge =
+      for n <- group |> hd() |> String.graphemes(),
+          String.contains?(Enum.fetch!(group, 1), n),
+          String.contains?(Enum.fetch!(group, 2), n),
+          do: n
+
+    convert_item(badge)
+  end
+
+  defp convert_item(letter) do
+    hd(letter)
+    |> String.to_charlist()
+    |> hd
+    |> to_priority()
+  end
+
+  defp to_priority(num) when num > 96, do: num - 96
+  defp to_priority(num), do: num - 38
 end
