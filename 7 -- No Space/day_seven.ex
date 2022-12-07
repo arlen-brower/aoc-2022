f8adbd17fd8af8f563ccef2fcaf71d6d1f8b9603
Arlen <arlen.brower@curtin.edu.au>
2022-12-07 22:04:00 +0800

    Day 7 -- Parts 1 and 2 (sorta, I feel like I'm cheating)

--- 7 -- No Space/day_seven.ex
+++ 7 -- No Space/day_seven.ex
@@ -1,12 +1,18 @@
 defmodule DaySeven do
   @threshold_size 100_000
+  @capacity 70_000_000
+  @required 30_000_000
+  @min 5_174_025
 
   def run(input_path \\ "test_input") do
-    lines =
-      File.read!(input_path)
-      |> String.split("\n", trim: true)
+    File.read!(input_path)
+    |> String.split("\n", trim: true)
+    |> do_parse()
+  end
 
-    parser(lines, 0, 0)
+  def do_parse(lines) do
+    {[], total, target} = parser(lines, 0, 0)
+    [total: total, target: target]
   end
 
   def parser([], total, target_total), do: {[], total, target_total}
@@ -15,9 +21,13 @@ defmodule DaySeven do
   def parser([cd | rest], total, target_total) do
     {unparsed, dir_size} =
       tl(rest)
-      |> list_dir([], 0)
+      |> list_dir(0)
 
     {next_lines, new_total, new_target_total} = parser(unparsed, dir_size, target_total)
+
+    if new_total >= @min do
+      IO.puts(new_total)
+    end
 
     if new_total <= @threshold_size do
       parser(next_lines, new_total + total, new_target_total + new_total)
@@ -26,10 +36,10 @@ defmodule DaySeven do
     end
   end
 
-  def list_dir([], files, dir_size), do: {[], dir_size}
-  def list_dir(["$" <> _ | _] = lines, files, dir_size), do: {lines, dir_size}
+  def list_dir([], dir_size), do: {[], dir_size}
+  def list_dir(["$" <> _ | _] = lines, dir_size), do: {lines, dir_size}
 
-  def list_dir([current | rest], files, dir_size) do
+  def list_dir([current | rest], dir_size) do
     file_size =
       case Regex.run(~r/(\d+) .*/, current, capture: :all_but_first) do
         [x] ->
@@ -39,6 +49,6 @@ defmodule DaySeven do
           0
       end
 
-    list_dir(rest, [current | files], file_size + dir_size)
+    list_dir(rest, file_size + dir_size)
   end
 end
