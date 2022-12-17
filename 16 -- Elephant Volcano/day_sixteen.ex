39506533a6cd20b3d562a8168601ad5476c50987
Arlen <arlen.brower@curtin.edu.au>
2022-12-17 12:13:08 +0800

    Day 16 -- This is hilariously awful, but it works! :-)

--- 16 -- Elephant Volcano/day_sixteen.ex
+++ 16 -- Elephant Volcano/day_sixteen.ex
@@ -1,5 +1,12 @@
 defmodule DaySixteen do
   @time 30
+  @training_time 4
+
+  def spam(file_path \\ "input", runs \\ 1000),
+    do:
+      Enum.reduce(1..runs, [], fn _run, acc -> [run(file_path) |> IO.inspect() | acc] end)
+      |> Enum.max()
+
   def run(file_path \\ "test_input") do
     file_path
     |> File.read!()
@@ -12,7 +19,73 @@ defmodule DaySixteen do
     dists = start_bfs(valves, :AA)
     pri = priority_list(dists)
 
-    next(dists, pri, [], @time, 0, 0)
+    # [part_one: next(dists, pri, [], @time, 0, 0), part_two: new_next(dists)]
+    new_next(dists)
+  end
+
+  def new_next(valves) do
+    dists = start_bfs(valves, :AA)
+    pri = priority_list(dists)
+
+    len = length(pri)
+
+    my_effort = %{valves: dists, time: @time - @training_time, rate: 0, total: 0}
+    el_effort = %{valves: dists, time: @time - @training_time, rate: 0, total: 0}
+
+    state = %{me: my_effort, ele: el_effort, open: []}
+
+    new_state =
+      Enum.reduce(1..len, state, fn _, cur_state ->
+        ele_time = cur_state.ele.time
+        me_time = cur_state.me.time
+
+        cur_state
+        |> player_move(:me)
+        |> player_move(:ele)
+      end)
+
+    new_state.me.time * new_state.me.rate + new_state.me.total +
+      new_state.ele.time * new_state.ele.rate + new_state.ele.total
+  end
+
+  def find_pri(_, [], _), do: []
+
+  def find_pri(valves, [best | rest], time) do
+    time_taken = valves[best].distance + 1
+
+    if time_taken > time do
+      find_pri(valves, rest, time)
+    else
+      best
+    end
+  end
+
+  def player_move(state, player) do
+    p = state[player]
+    open = state.open
+    pri = p.valves |> priority_list(open)
+    pri = find_pri(p.valves, pri, p.time)
+
+    unless pri == [] do
+      best = pri
+      time_taken = p.valves[best].distance + 1
+
+      new_v = start_bfs(p.valves, best)
+      flow_rate = p.valves[best].rate
+
+      updated_player = %{
+        valves: new_v,
+        time: p.time - time_taken,
+        rate: flow_rate + p.rate,
+        total: p.total + p.rate * time_taken
+      }
+
+      state
+      |> Map.put(player, updated_player)
+      |> Map.put(:open, [best | open])
+    else
+      state
+    end
   end
 
   def next(_valves, _pri, _open, 0, _flow_rate, total), do: total
@@ -21,13 +94,16 @@ defmodule DaySixteen do
     do: next(valves, [], open, time - 1, flow_rate, total + flow_rate)
 
   def next(valves, [best | rest], open, time, flow_rate, total) do
-    IO.puts("Remaining: #{length(rest)}")
     # Travel time and opening time
     time_taken = valves[best].distance + 1
 
     if time_taken < time do
       dists = start_bfs(valves, best)
-      pri = dists |> priority_list() |> Enum.reject(fn x -> x in open end)
+
+      pri =
+        dists
+        |> priority_list()
+        |> Enum.reject(fn x -> x in open end)
 
       next(
         dists,
@@ -42,19 +118,32 @@ defmodule DaySixteen do
     end
   end
 
-  def priority_list(valves) do
-    valves
-    |> Enum.map(fn {label, valve} ->
-      {label,
-       unless valve.distance == 0 do
-         valve.rate / valve.distance
-       else
-         0
-       end}
+  def priority_list(valves, reject \\ []) do
+    pri_list =
+      valves
+      |> Enum.map(fn {label, valve} ->
+        {label,
+         unless valve.distance == 0 do
+           valve.rate / valve.distance + :rand.uniform(3)
+         else
+           0
+         end}
+      end)
+      |> Enum.sort_by(fn {_, dist} -> dist end, :desc)
+      |> Enum.reject(fn {_, dist} -> dist == 0 end)
+      |> Enum.map(fn {label, _} -> label end)
+      |> Enum.reject(fn x -> x in reject end)
+  end
+
+  def bfs_dist(valves, start, [], distance), do: distance + valves[start].distance
+
+  def bfs_dist(valves, start, goals, distance) do
+    dists = start_bfs(valves, start)
+
+    Enum.map(goals, fn st ->
+      bfs_dist(dists, st, goals -- [st], valves[st].distance + distance)
     end)
-    |> Enum.sort_by(fn {_, dist} -> dist end, :desc)
-    |> Enum.reject(fn {_, dist} -> dist == 0 end)
-    |> Enum.map(fn {label, _} -> label end)
+    |> Enum.min()
   end
 
   def start_bfs(valves, start) do
