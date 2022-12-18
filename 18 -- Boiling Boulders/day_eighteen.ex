866925e2ba2d7fc39d830fe9318513fdd998b25e
Arlen <arlen.brower@curtin.edu.au>
2022-12-18 16:59:57 +0800

    Day 18 -- Both parts.

--- 18 -- Boiling Boulders/day_eighteen.ex
+++ 18 -- Boiling Boulders/day_eighteen.ex
@@ -9,69 +9,142 @@ defmodule DayEighteen do
 
     p1 = cubes |> surface_area()
 
-    air_points =
-      cubes
-      |> Enum.reduce(MapSet.new(), fn pt, acc -> get_air_set(cubes, pt) |> MapSet.union(acc) end)
-      |> Enum.filter(fn pt -> inside?(cubes, pt) end)
-      |> IO.inspect()
-      |> Enum.count()
+    {solid_cube, x_bounds, y_bounds, z_bounds, start_point} = find_box(cubes)
+    node_map = points_to_nodes(solid_cube)
+    filter_fun = &(not MapSet.member?(cubes, &1) and in_bounds(&1, x_bounds, y_bounds, z_bounds))
 
-    [part_one: p1, part_two: p1 - air_points * 6]
+    p2 =
+      start_bfs(node_map, start_point, filter_fun)
+      |> Enum.reject(fn {_, node} -> node.visited end)
+      |> nodes_to_points()
+      |> surface_area()
+
+    [part_one: p1, part_two: p2]
   end
 
-  def inside?(cubes, {air_x, air_y, air_z}) do
-    x = Enum.map(cubes, fn {x, _, _} -> x end)
-    y = Enum.map(cubes, fn {_, y, _} -> y end)
-    z = Enum.map(cubes, fn {_, _, z} -> z end)
-
-    x_min = Enum.min(x)
-    x_max = Enum.max(x)
-    y_min = Enum.min(y)
-    y_max = Enum.max(y)
-    z_min = Enum.min(z)
-    z_max = Enum.max(z)
-
-    left = Enum.map(x_min..air_x, fn x -> {x, air_y, air_z} in cubes end)
-    right = Enum.map(air_x..x_max, fn x -> {x, air_y, air_z} in cubes end)
-
-    down = Enum.map(y_min..air_y, fn y -> {air_x, y, air_z} in cubes end)
-    up = Enum.map(air_y..y_max, fn y -> {air_x, y, air_z} in cubes end)
-
-    backward = Enum.map(air_z..z_max, fn z -> {air_x, air_y, z} in cubes end)
-    forward = Enum.map(z_min..air_z, fn z -> {air_x, air_y, z} in cubes end)
-
-    Enum.any?(left) and Enum.any?(right) and Enum.any?(up) and Enum.any?(down) and
-      Enum.any?(forward) and Enum.any?(backward)
+  def in_bounds({x, y, z}, xbounds, ybounds, zbounds) do
+    x in xbounds and y in ybounds and z in zbounds
   end
 
   def surface_area(cubes) do
     Enum.reduce(cubes, 0, fn cube, acc -> acc + count_exposed(cubes, cube) end)
   end
 
-  def get_air_set(cubes, {x, y, z}) do
-    air_points =
-      [
-        {x, y + 1, z},
-        {x, y - 1, z},
-        {x - 1, y, z},
-        {x + 1, y, z},
-        {x, y, z + 1},
-        {x, y, z - 1}
-      ]
+  def find_box(cubes) do
+    x = Enum.map(cubes, fn {x, _, _} -> x end)
+    y = Enum.map(cubes, fn {_, y, _} -> y end)
+    z = Enum.map(cubes, fn {_, _, z} -> z end)
+
+    x_min = Enum.min(x)
+    x_max = Enum.max(x)
+
+    y_min = Enum.min(y)
+    y_max = Enum.max(y)
+
+    z_min = Enum.min(z)
+    z_max = Enum.max(z)
+
+    cube_list =
+      for x <- x_min..x_max, do: for(y <- y_min..y_max, do: for(z <- z_min..z_max, do: {x, y, z}))
+
+    cube_map =
+      cube_list
+      |> List.flatten()
       |> MapSet.new()
-      |> MapSet.difference(cubes)
+
+    {cube_map, x_min..x_max, y_min..y_max, z_min..z_max, {x_min, y_min, z_min}}
+  end
+
+  def points_to_nodes(cubes) do
+    Enum.reduce(cubes, %{}, fn label, acc ->
+      Map.put(acc, label, %{visited: false, distance: :inf, label: label})
+    end)
+  end
+
+  def nodes_to_points(nodes) do
+    Enum.reduce(nodes, MapSet.new(), fn {label, _}, acc -> MapSet.put(acc, label) end)
+  end
+
+  def start_bfs(nodes, start, filter_fun) do
+    nodes[start]
+    |> Map.put(:visited, true)
+    |> Map.put(:distance, 0)
+    |> update_nodes(start, nodes)
+    |> bfs([start], filter_fun)
+  end
+
+  def bfs(nodes, [], _), do: nodes
+
+  def bfs(nodes, q, filter_fun) do
+    s = q_front(q)
+    q = q_pop(q)
+
+    {new_nodes, new_q} =
+      Enum.reduce(filter_neighbours(nodes[s], filter_fun), {nodes, q}, fn u, {v_acc, q_acc} ->
+        unless v_acc[u].visited do
+          updated_v =
+            v_acc[u]
+            |> Map.put(:visited, true)
+            |> Map.put(:distance, v_acc[s].distance + 1)
+            |> update_nodes(u, v_acc)
+
+          {updated_v, q_push(q_acc, u)}
+        else
+          {v_acc, q_acc}
+        end
+      end)
+
+    bfs(new_nodes, new_q, filter_fun)
+  end
+
+  def q_front(queue), do: List.first(queue)
+  def q_push(queue, label), do: queue ++ [label]
+
+  def q_pop(queue) do
+    {_, new_q} = List.pop_at(queue, 0)
+    new_q
+  end
+
+  def update_nodes(node, label, nodes) do
+    Map.put(nodes, label, node)
+  end
+
+  def reset_visited(nodes, ignore \\ []) do
+    Enum.reduce(nodes, nodes, fn {label, node}, acc ->
+      if label in ignore do
+        acc
+      else
+        update_nodes(%{node | visited: false}, label, acc)
+      end
+    end)
+  end
+
+  def filter_neighbours(%{label: {x, y, z}}, filter_fun) do
+    [
+      {x, y + 1, z},
+      {x, y - 1, z},
+      {x - 1, y, z},
+      {x + 1, y, z},
+      {x, y, z + 1},
+      {x, y, z - 1}
+    ]
+    |> Enum.filter(filter_fun)
+  end
+
+  def neighbours({x, y, z}) do
+    [
+      {x, y + 1, z},
+      {x, y - 1, z},
+      {x - 1, y, z},
+      {x + 1, y, z},
+      {x, y, z + 1},
+      {x, y, z - 1}
+    ]
   end
 
   def count_exposed(cubes, {x, y, z}) do
     covered_sides =
-      [
-        {x, y + 1, z},
-        {x, y - 1, z},
-        {x - 1, y, z},
-        {x + 1, y, z},
-        {x, y, z + 1},
-        {x, y, z - 1}
-      ]
+      neighbours({x, y, z})
       |> MapSet.new()
       |> MapSet.intersection(cubes)
       |> Enum.count()
